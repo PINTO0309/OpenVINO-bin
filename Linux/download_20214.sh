12f74e5a4dc201d1a76758adbc14d0a5b65dd2e7
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-07-03 12:18:10 +0900

    Update download_20214.sh

--- Linux/download_20214.sh
+++ Linux/download_20214.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1sKAaJfMbZ9ISiH6PHl6m6bZNQZGdwl5X" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=18PQFD5PTfPfIVq965OIl_HyM_4x-x9m8" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1sKAaJfMbZ9ISiH6PHl6m6bZNQZGdwl5X" -o l_openvino_toolkit_p_2021.4.582.tgz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=18PQFD5PTfPfIVq965OIl_HyM_4x-x9m8" -o l_openvino_toolkit_p_2021.4.582.tgz
 tar -zxvf l_openvino_toolkit_p_2021.4.582.tgz
 rm l_openvino_toolkit_p_2021.4.582.tgz
